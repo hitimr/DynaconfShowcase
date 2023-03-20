e8c5e4c1a8504353b1999fa758ee36a1e5a68bf6
hitimr <mrh@sinntelligence.ai>
2023-03-20 12:04:09 +0100

    exporting

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 from config.config import settings
+from dynaconf.utils.boxing import DynaBox
+from dynaconf import loaders
 
 # Read some settings
 print(f"current config: {settings.CURRENT_CONFIG}")
@@ -6,3 +8,5 @@ print(f"host: {settings.TCP_SERVER_PORT}")
 
 # Invoke validators manually
 settings.validators.validate_all()
+
+loaders.write('out.toml', settings.as_dict(), env="testing")
