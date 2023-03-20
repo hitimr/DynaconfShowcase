c296b4ed4515ff0d36e913b7aefe933981177a70
hitimr <mrh@sinntelligence.ai>
2023-03-20 16:53:45 +0100

    typo

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from config.config import settings
 
 # Read some settings
 print(f"current config: {settings.CURRENT_CONFIG}")
-print(f"host: {settings.TCP_SERVER_PORT}")
+print(f"host: {settings.TCP_SERVER_HOST}")
 
 # Invoke validators manually
 settings.validators.validate_all()
