2ac85aa46dc1c293f6267d24cd6bafd9f09fe0ac
hitimr <mrh@sinntelligence.ai>
2023-03-20 11:43:44 +0100

    moved some files

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from config.config import settings
+
+# Read some settings
+print(f"current config: {settings.CURRENT_CONFIG}")
+print(f"host: {settings.TCP_SERVER_PORT}")
+
+# Invoke validators manually
+settings.validators.validate_all()
