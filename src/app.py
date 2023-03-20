ab250d5f945798089c9134cc81fa3ec74e736ef9
hitimr <mrh@sinntelligence.ai>
2023-03-20 12:05:09 +0100

    removed write

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
 from config.config import settings
-from dynaconf.utils.boxing import DynaBox
-from dynaconf import loaders
 
 # Read some settings
 print(f"current config: {settings.CURRENT_CONFIG}")
@@ -8,5 +6,3 @@ print(f"host: {settings.TCP_SERVER_PORT}")
 
 # Invoke validators manually
 settings.validators.validate_all()
-
-loaders.write('out.toml', settings.as_dict(), env="testing")
